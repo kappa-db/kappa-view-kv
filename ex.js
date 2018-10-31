d33eed2bb196c79474e2fa825c9feb03fe027d11
noffle <sww@eight45.net>
2018-10-31 13:53:54 -0700

    basic impl

---
+++ ex.js
@@ -0,0 +1,42 @@
+var memdb = require('memdb')
+var kappa = require('kappa-core')
+var kv = require('.')
+
+var core = kappa('./db', { valueEncoding: 'json' })
+var idx = memdb()
+
+var kvIdx = kv(idx, function (msg, next) {
+  if (!msg.value.id) return next()
+  var ops = []
+  var msgId = msg.key + '@' + msg.seq
+  ops.push({ key: msg.value.id, id: msgId, links: msg.value.links || [] })
+  next(null, ops)
+})
+core.use('kv', kvIdx)
+
+core.feed('local', function (err, feed) {
+  var docs = [
+    { id: 'foo' },
+    { id: 'foo', n: 3, links: [] },
+    { id: 'foo', n: 12, links: [] }
+  ]
+  feed.append(docs[0], function (err, seq) {
+    docs[1].links.push(version(feed, seq))
+    feed.append(docs[1], function () {
+      docs[2].links.push(version(feed, seq))
+      feed.append(docs[2], function () {
+        core.api.kv.get('foo', function (err, values) {
+          console.log('values', values)
+        })
+      })
+    })
+  })
+
+  core.api.kv.onUpdate('foo', function (msg) {
+    console.log('update', msg)
+  })
+})
+
+function version (feed, seq) {
+  return feed.key.toString('hex') + '@' + seq
+}
---
+++ index.js
@@ -0,0 +1,94 @@
+var umkv = require('unordered-materialized-kv')
+var EventEmitter = require('events').EventEmitter
+
+module.exports = KV
+
+function KV (db, mapFn, opts) {
+  var kv = umkv(db)
+  var events = new EventEmitter()
+  opts = opts || {}
+
+  var idx = {
+    maxBatch: opts.maxBatch || 100,
+
+    map: function (msgs, next) {
+      var allOps = []
+      var pending = msgs.length + 1
+      for (var i = 0; i < msgs.length; i++) {
+        var msg = msgs[i]
+        mapFn(msg, function (err, ops) {
+          done(err, ops, msg)
+        })
+      }
+      done(null, [])
+
+      function done (err, ops, msg) {
+        if (err) {
+          pending = Infinity
+          return next(err)
+        }
+        allOps.push.apply(allOps, ops)
+        if (!--pending) kv.batch(allOps, next)
+      }
+    },
+
+    indexed: function (msgs) {
+      for (var i = 0; i < msgs.length; i++) {
+        mapFn(msgs[i], function (err, ops) {
+          if (err) return
+          events.emit('update!' + ops[0].key, msgs[i])
+        })
+      }
+    },
+
+    api: {
+      get: function (core, key, cb) {
+        this.ready(function () {
+          kv.get(key, function (err, ids) {
+            if (err) return cb(err)
+            var res = []
+            var pending = ids.length + 1
+            for (var i = 0; i < ids.length; i++) {
+              var id = ids[i]
+              // XXX: this assumes KEY@SEQ format!!! userspace might not do
+              // this!
+              // TODO: expose this on kappa-core somehow
+              // TODO: in fact, can we just expose the 'version' string /w @
+              // explicitly?
+              var feed = core._logs.feed(id.split('@')[0])
+              feed.get(id.split('@')[1], done)
+            }
+            done()
+
+            function done (err, msg) {
+              if (err) {
+                pending = Infinity
+                return cb(err)
+              }
+              if (msg) res.push(msg)
+              if (!--pending) cb(null, res)
+            }
+          })
+        })
+      },
+
+      onUpdate: function (core, key, cb) {
+        events.on('update!' + key, cb)
+      }
+    },
+
+    storeState: function (state, cb) {
+      db.put('state', state, cb)
+    },
+
+    fetchState: function (cb) {
+      db.get('state', function (err, state) {
+        if (err && err.notFound) cb()
+        else if (err) cb(err)
+        else cb(null, state)
+      })
+    },
+  }
+  return idx
+}
+
